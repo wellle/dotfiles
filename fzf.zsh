a0dcc447a48b4735163e1111b3dcb2e1124495e8
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2014-07-01 11:52:27 +0200

    Add fuzzy tmux session selection

--- fzf.zsh
+++ fzf.zsh
@@ -71,3 +71,13 @@ fda() {
 fh() {
   eval $(('fc' -l 1 || 'history') | fzf +s | sed 's/ *[0-9]* *//')
 }
+
+# fs [FUZZY PATTERN] - Select selected tmux session
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fs() {
+  local session
+  session=$(tmux list-sessions -F "#{session_name}" | \
+    fzf --query="$1" --select-1 --exit-0) &&
+  tmux switch-client -t "$session"
+}
